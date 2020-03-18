9bab04d9811b149b91a721019a05482d9865b8e1
FlynnEastwood <aflynneastwood@gmail.com>
2020-03-18 10:31:31 -0400

    Update RenderBool.py
    Made the RenderBool packaged into a addon.

--- RenderBool.py
+++ RenderBool.py
@@ -1,13 +1,66 @@
+bl_info = {
+    "name": "TonyTools",
+    "author": "Antoine Flynn",
+    "version": (1, 0),
+    "blender": (2, 82, 0),
+    "location": "View3D > Tab > Tony Tools",
+    "description": "Workflow enhancer",
+    "warning": "",
+    "wiki_url": "",
+    "category": "Tools",
+}
+
 import bpy
 
-bpy.context.selected_objects
 
-bpy.context.object.hide_render = True
-bpy.context.object.cycles_visibility.camera = False
-bpy.context.object.cycles_visibility.transmission = False
-bpy.context.object.cycles_visibility.diffuse = False
-bpy.context.object.cycles_visibility.scatter = False
-bpy.context.object.cycles_visibility.glossy = False
-bpy.context.object.cycles_visibility.shadow = False
+def main():
 
-bpy.context.object.display_type = 'WIRE'
+    bpy.context.selected_objects
+
+    bpy.context.object.hide_render = True
+    bpy.context.object.cycles_visibility.camera = False
+    bpy.context.object.cycles_visibility.transmission = False
+    bpy.context.object.cycles_visibility.diffuse = False
+    bpy.context.object.cycles_visibility.scatter = False
+    bpy.context.object.cycles_visibility.glossy = False
+    bpy.context.object.cycles_visibility.shadow = False
+
+    bpy.context.object.display_type = 'WIRE'
+    
+class RenderBool(bpy.types.Operator):
+    '''Tooltip'''
+    bl_idname= 'myops.render_bool'
+    bl_label = 'Set Bool Render'
+    
+    
+    def execute(self, context):
+        main()
+        return {"FINISHED"}
+
+class TonyTools_Panel(bpy.types.Panel):
+    """Creates a Panel in the Tool shelf"""
+    bl_label = "Time Savers"
+    bl_idname = "OBJECT_PT_RenderBool"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'Tony Tools'
+
+    def draw(self, context):
+        layout = self.layout
+
+        row = layout.row()
+        row.operator("myops.render_bool")
+        return {"FINISHED"}
+    
+def register():
+    bpy.utils.register_class(RenderBool)
+    bpy.utils.register_class(TonyTools_Panel)
+
+def unregister():
+    bpy.utils.unregister_class(RenderBool)
+    bpy.utils.unregister_class(TonyTools_Panel)
+    
+    
+if __name__ == "__main__":
+    register()
+  
