44fe4e05b475339bf2d7dd0694b7c9f0684f69aa
FlynnEastwood <af93@protonmail.com>
2021-05-23 17:16:33 -0400

    Multiple format supported and folders
    Script will now create folder structures and export models but to root still.

--- FormatsExporter.py
+++ FormatsExporter.py
@@ -4,6 +4,7 @@ import os
 #Get the blender file directory
 basedir = os.path.dirname(bpy.data.filepath)
 
+
 #Get initial location and resets it before export
 selection =  bpy.context.active_object
 initialLocation = selection.location.copy()
@@ -13,7 +14,26 @@ selection.location.xyz = 0
 name = bpy.path.clean_name(selection.name)
 fn = os.path.join(basedir, name)
 
-
+dae_path = os.path.join(basedir, "DAE") 
+if not os.path.exists(dae_path):
+    os.makedirs(dae_path)
+    
+fbx_path = os.path.join(basedir, "FBX") 
+if not os.path.exists(fbx_path):
+    os.makedirs(fbx_path)
+    
+gltf_path = os.path.join(basedir, "GLTF") 
+if not os.path.exists(gltf_path):
+    os.makedirs(gltf_path)
+    
+obj_path = os.path.join(basedir, "OBJ") 
+if not os.path.exists(obj_path):
+    os.makedirs(obj_path)
+    
+stl_path = os.path.join(basedir, "STL") 
+if not os.path.exists(stl_path):
+    os.makedirs(stl_path)
+    
 #Export as COLLADA
 bpy.ops.wm.collada_export(filepath=fn)
 #Export as FBX
@@ -29,5 +49,3 @@ bpy.ops.export_scene.gltf(filepath=fn + ".gltf", use_selection=True)
 
 #Set model nack to initial location
 selection.location.xyz = initialLocation
-
-
